9cd57928897ad18c701eccc34e66ba12f6e62176
leonid-shutov <leonid.shutov@hey.com>
2026-04-09 22:10:34 +0200

    update

--- Formula/tuiwarden.rb
+++ Formula/tuiwarden.rb
@@ -1,5 +1,5 @@
 class Tuiwarden < Formula
-  desc "Bitwarden terminal UI"
+  desc "Bitwarden terminal UI."
   homepage "https://github.com/leonid-shutov/tuiwarden"
   url "https://github.com/leonid-shutov/tuiwarden/archive/refs/tags/v1.0.0-alpha.1.tar.gz"
   sha256 "8ebfd4083fb876d68435a2b33614053bd597f7ef46e45aee75480b4e72f6b988"
